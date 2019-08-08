05ff836011e29c97a8c62144ccdb250f96e59fbd
luongquochai <51089030+luongquochai@users.noreply.github.com>
2019-08-09 02:24:48 +0700

    update

---
+++ public/ctrler.js
@@ -0,0 +1,42 @@
+class Hello extends React.Component {
+  render() {
+    return (
+      <div>
+        <h3> Author: Quoc Hai </h3>
+        <h1> Hello world ! </h1>
+        <h1> Click Button to Toggle ! </h1>
+        <Toggle />
+      </div>
+    );
+  }
+}
+
+class Toggle extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {isToggleOn: true};
+
+    // This binding is necessary to make `this` work in the callback
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState(state => ({
+      isToggleOn: !state.isToggleOn
+    }));
+  }
+
+  render() {
+    return (
+      <button onClick={this.handleClick}>
+        {this.state.isToggleOn ? 'ON' : 'OFF'}
+      </button>
+    );
+  }
+}
+
+ReactDOM.render(
+  <div>
+  <Hello />
+  </div>,
+  document.getElementById("root"));
---
+++ readme.txt
@@ -0,0 +1,23 @@
+_Cài đặt nodejs cho server để render ra html cho reactjs
+-Cài đặt pakage.json //npm init
+-Cài module express (module render server trong nền nodejs), ejs 
+-Tao file index.js (nằm phía sv)->mục đích render ra html
+
+--nhúng react vào và render ra html
+https://cdnjs.com/libraries/react/15.4.0
+
+-cài đặt reactjs cdn trên reactjs.org (2script)
+-cai dat babel core để render html cho dễ
+/div: hứng kq của render
+
+- JSX từ babel-core
+
+-COMPONENT: đối tượng quan trọng nhất
+
+đối tượng được sử dụng đi nhiều lần thì chúng ta khai báo nó là 1 component, 
+1 component có nhiều trạng thái
+render: 1 hđ trong js
+
+ES6: 2015 khai báo class thay vì dùng function
+-constructor: hàm để tạo ra các thuộc tính
+-super: ghi lại thuộc tính của cha
